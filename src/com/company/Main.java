b7a29f63eaaa86e13e8b0d70d857bde468f6ef7e
Marsel151205 <mambetalievmarsel2289@gmail.com>
2022-03-19 15:41:35 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int[] array = {5, 6, 7, 1, 2};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+}
+
+
