939150e15b5769e0addebebf717a06f4e82e4dfc
Marsel151205 <mambetalievmarsel2289@gmail.com>
2022-03-20 01:02:22 +0300

    /

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,10 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         int[] array = {5, 6, 7, 1, 2};
+        System.out.println(Arrays.toString(array));
         Arrays.sort(array);
         System.out.println(Arrays.toString(array));
+
 
     }
 }
